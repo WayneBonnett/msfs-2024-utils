bd7295c6145133a74b9ba77bcddd20bdc7342595
Leftos Aslanoglou <leftos@gmail.com>
2024-12-20 17:27:00 -0800

    simbrief_p2_calc_auto: Add interactive options for passenger and freight calculations, including OFP update functionality

--- simbrief_p2_calc/simbrief_p2_calc_auto.py
+++ simbrief_p2_calc/simbrief_p2_calc_auto.py
@@ -19,11 +19,13 @@ parser.add_argument("--username", type=str, default=None)
 parser.add_argument("--airframe", type=str, default=None)
 parser.add_argument("--desired_pax", type=int, default=None)
 parser.add_argument("--desired_freight", type=int, default=None)
+parser.add_argument("--update", type=int, nargs=2, default=None)
 args = parser.parse_args()
 
 if args.desired_pax is not None and args.desired_freight is not None:
     raise ValueError("You can't specify both desired_pax and desired_freight")
 
+any_args = args.username is not None or args.airframe is not None or args.desired_pax is not None or args.desired_freight is not None
 if args.username is None:
     # Prompt the user for their SimBrief username
     args.username = input("Enter your SimBrief username: ")
@@ -44,6 +46,36 @@ if args.airframe != "":
     if airframe is None:
         raise ValueError(f"Unknown airframe: {args.airframe}")
     max_pax = int(airframe["max_pax"])
+
+if not any_args and args.desired_pax is None and args.desired_freight is None:
+    while True:
+        print("Select one of the following:")
+        print("(1) Calculate max number of passengers and freight based on max passengers")
+        print("(2) Calculate max freight based on desired number of passengers")
+        print("(3) Calculate max number of passengers based on desired freight")
+        print("(4) Update Simbrief OFP with final values from P2")
+        input_value = input("Select option: ")
+        if input_value == "1":
+            break
+        elif input_value == "2":
+            args.desired_pax = int(input("Enter the desired number of passengers: "))
+            break
+        elif input_value == "3":
+            args.desired_freight = int(input("Enter the desired freight: "))
+            break
+        elif input_value == "4":
+            pax = int(input("Enter the final number of passengers: "))
+            freight = int(input("Enter the final freight: "))
+            args.update = (pax, freight)
+            break
+
+if args.update:
+    simbrief_dispatch_update_url = f"https://dispatch.simbrief.com/options/latest?pax={args.update[0]}&cargo={args.update[1]/1000.0}"
+    # launch default browser with the URL
+    import webbrowser
+    webbrowser.open(simbrief_dispatch_update_url)
+    import sys
+    sys.exit(0)
 
 # get the latest simbrief ofp json
 simbrief_ofp_url = f"https://www.simbrief.com/api/xml.fetcher.php?username={args.username}&json=1"
